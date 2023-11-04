057e213033559ddf4e3432a4301448fe77ee777b
DJLad97 <dj-scott1@hotmail.co.uk>
2023-11-04 15:28:22 +0000

    Created card component

---
+++ src/components/Card.vue
@@ -0,0 +1,12 @@
+<template>
+    <div
+        class="card bg-neutral text-neutral-content drop-shadow-xl sm:w-96 w-full mx-4"
+    >
+        <div class="card-body">
+            <h1 class="text-center">
+                <slot name="title" />
+            </h1>
+            <slot name="content" />
+        </div>
+    </div>
+</template>
