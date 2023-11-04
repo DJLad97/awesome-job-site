e1768882da96993f00f6fe7a74dce56a3886108c
DJLad97 <dj-scott1@hotmail.co.uk>
2023-11-04 14:09:49 +0000

    Create text input component

---
+++ src/components/form/TextInput.vue
@@ -0,0 +1,31 @@
+<template>
+    <input
+        v-model="input"
+        :type="type"
+        class="input input-bordered w-full max-w-xs"
+        :name="name"
+        :id="id"
+        :aria-invalid="ariaInvalid"
+        aria-errormessage="ariaErrormessage"
+    />
+</template>
+
+<script lang="ts" setup>
+import { defineModel } from 'vue';
+type Props = {
+    id: string;
+    name: string;
+    type?: string;
+    ariaInvalid?: boolean;
+    ariaErrormessage: string;
+};
+
+withDefaults(defineProps<Props>(), {
+    type: 'text',
+    ariaInvalid: false,
+});
+
+const input = defineModel<string>('input', {
+    required: true,
+});
+</script>
