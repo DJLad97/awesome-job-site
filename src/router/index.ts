86f1f56332847f8213fa9a0887ee11e84e58d863
DJLad97 <dj-scott1@hotmail.co.uk>
2023-11-04 18:00:58 +0000

    Added routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,26 @@
 import { createWebHistory, createRouter } from 'vue-router';
+import Home from '@/views/Home.vue';
+import Login from '@/views/Login.vue';
+import Register from '@/views/Register.vue';
+import RoutePaths from './routes';
 
-const routes = [];
+const routes = [
+    {
+        path: RoutePaths.Home,
+        name: 'Home',
+        component: Home,
+    },
+    {
+        path: RoutePaths.Login,
+        name: 'Login',
+        component: Login,
+    },
+    {
+        path: RoutePaths.Register,
+        name: 'Register',
+        component: Register,
+    },
+];
 
 const router = createRouter({
     history: createWebHistory(),
