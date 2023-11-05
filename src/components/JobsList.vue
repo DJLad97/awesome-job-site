db13d1b997aca8fcd4c204d31884f75e73a3085c
DJLad97 <dj-scott1@hotmail.co.uk>
2023-11-05 16:16:03 +0000

    Add job search functionality

--- src/components/JobsList.vue
+++ src/components/JobsList.vue
@@ -1,10 +1,45 @@
 <template>
+    <div class="form-control">
+        <div class="input-group">
+            <input
+                v-model="searchValue"
+                id="job-search"
+                name="job-search"
+                type="text"
+                placeholder="Search…"
+                class="input input-bordered w-full"
+            />
+            <button class="btn btn-square">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                >
+                    <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                    />
+                </svg>
+            </button>
+        </div>
+    </div>
     <div
-        v-for="job in jobs"
+        v-for="job in filteredJobs"
         :key="job.id"
         class="collapse collapse-arrow bg-base-200 w-fit my-4"
     >
-        <input type="radio" name="job" />
+        <input
+            type="checkbox"
+            :id="`job-${job.id}`"
+            :name="`job-${job.id}`"
+            :aria-label="job.title"
+            :aria-describedby="`job-description-${job.id}`"
+            aria-role="accordion"
+        />
         <div
             class="collapse-title text-xl font-medium"
             :aria-describedby="`job-title-${job.id}`"
@@ -13,7 +48,7 @@
             <span class="block text-xs text-accent">{{ job.company }} </span>
         </div>
         <div class="collapse-content">
-            {{ job.description }}
+            <span :id="`job-description-${job.id}`">{{ job.description }}</span>
             <button
                 type="button"
                 class="mt-4 block btn btn-primary btn-sm"
@@ -33,15 +68,29 @@ import { useRouter } from 'vue-router';
 import { storeToRefs } from 'pinia';
 import RoutesPaths from '@/router/routes';
 import { Job } from '@/stores/jobs/types';
+import { computed, ref } from 'vue';
 
 const router = useRouter();
 const userStore = useUserStore();
 
-defineProps<{
+const props = defineProps<{
     jobs: Array<Job>;
 }>();
 
 const { getUserJobsbyId } = storeToRefs(userStore);
+
+const searchValue = ref('');
+const filteredJobs = computed(() => {
+    const filteredJobs = props.jobs.filter((job) => {
+        return job.title
+            .toLowerCase()
+            .includes(searchValue.value.toLowerCase());
+    });
+
+    console.log(filteredJobs);
+
+    return filteredJobs.length > 0 ? filteredJobs : props.jobs;
+});
 
 function goToJob(id: number) {
     router.push(`${RoutesPaths.Job}/${id}`);
